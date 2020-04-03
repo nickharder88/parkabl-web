4a85abf60f1e4d2b7655e2a625ded35dad19e76f
Nick Harder <nick@abstraconsulting.com>
2020-04-02 21:15:46 -0400

    change to dark theme

--- src/theme.js
+++ src/theme.js
@@ -4,19 +4,17 @@ import { createMuiTheme } from '@material-ui/core/styles';
 // A custom theme for this app
 const theme = createMuiTheme({
   palette: {
+    type: 'dark',
     primary: {
-      main: '#556cd6',
+      main: '#556cd6'
     },
     secondary: {
-      main: '#19857b',
+      main: '#19857b'
     },
     error: {
-      main: red.A400,
-    },
-    background: {
-      default: '#fff',
-    },
-  },
+      main: red.A400
+    }
+  }
 });
 
 export default theme;
